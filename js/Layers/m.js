6c577b4e797d201692ed2f15f91c15a354444864
patfre <56884994+patfr@users.noreply.github.com>
2021-10-12 14:14:27 +0200

    Rebalancing

--- js/Layers/m.js
+++ js/Layers/m.js
@@ -7,12 +7,12 @@ addLayer("m", {
 		points: new Decimal(0),
     }},
     color: "#a212fc",
-    requires: new Decimal(5), // Can be a function that takes requirement increases into account
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "Multiplier", // Name of prestige currency
     baseResource: "Cash", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
+    exponent: 0.4, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
